9b101a4bf61fa2dd82640b6f57bf1b6e10e3ed53
Chiemelie <ibeabuchichiemelie01@gmail.com>
2023-12-06 14:33:50 +0100

    appointment page back button responsive

--- js/logindemo.js
+++ js/logindemo.js
@@ -1,1 +1,16 @@
 // To toggle between login and signup form
+const login = document.querySelector('.login'),
+signup = document.querySelector('.signup'),
+signupButton = document.querySelector('#signupbtn'),
+loginButton = document.querySelector('#loginbtn');
+
+signupButton.addEventListener('click', function(){
+    if(signup.style.display === 'none'){
+        login.style.display = 'block';
+    }
+    else{
+        signup.style.display = 'block';
+        login.style.display = 'none';
+    }
+})
+
