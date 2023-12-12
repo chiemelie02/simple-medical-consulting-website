56e4caa4ef2f8f4d107b9b0602fcc3cc01a5c9b8
Chiemelie <ibeabuchichiemelie01@gmail.com>
2023-12-13 00:05:09 +0100

    added navbar functionality, made a lot of adjustments, started hospital page

--- js/carousel.js
+++ js/carousel.js
@@ -4,7 +4,7 @@ owl.owlCarousel({
     loop:true,
     margin:40,
     autoplay:true,
-    autoplayTimeout:3000,
+    autoplayTimeout:4000,
     autoplayHoverPause:true,
-    responsive: {0:{items:1}, 768:{items:2}, 1024:{items:4}}
+    responsive: {0:{items:1}, 768:{items:2}, 1024:{items:3}}
 });
